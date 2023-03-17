857ed5bca86e8042660ed816d7efd5962fed1192
Brian Kim <briankimpossible@gmail.com>
2023-03-17 01:30:54 -0700

    messing with "tests"

--- poop.ts
+++ poop.ts
@@ -2,9 +2,11 @@ import {jsx} from "@b9g/crank/standalone";
 import {renderer} from "@b9g/crank/html";
 import noop from "./noop.ts";
 
+import {css} from "@emotion/css";
 export default {
 	fetch(req: Request) {
 		noop();
+		console.log(css);
 		//throw new Error("Poop");
 		const html = renderer.render(jsx`<div>Hello from Crank</div>`);
 		return new Response(html, {
--- test/poop.ts
+++ test/poop.ts
@@ -7,8 +7,8 @@ export default {
 	fetch(req: Request) {
 		noop();
 		console.log(css);
-		//throw new Error("Poop");
 		const html = renderer.render(jsx`<div>Hello from Crank</div>`);
+
 		return new Response(html, {
 			headers: {"content-type": "text/html; charset=UTF-8"},
 		});
